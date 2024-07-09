4e4cb4b2485220624d3465a99615afdee85de3f3
Priya <priyamaheshwari183@gmail.com>
2024-07-10 02:09:38 +0530

    added css to the grid

--- ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a9e1ebd59114b8503dd02bf7227d8a219403ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0149e56327b63a4cf3bc60515ad9f7c8f4c563")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
