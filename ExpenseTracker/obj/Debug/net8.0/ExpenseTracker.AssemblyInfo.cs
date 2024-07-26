e62fa0d0819d12283021fb1fe343dc447d64ae57
Priya <priyamaheshwari183@gmail.com>
2024-07-27 01:30:54 +0530

    created menu in side bar

--- ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/obj/Debug/net8.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524e9dac282bec40eccec88e0b4cc98a00daaac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742d57b44ef402144f5429c7895c184439a9b00d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
